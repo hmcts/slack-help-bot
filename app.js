8922deed2487ec5521de8cb04b71f201c5925959
Tim Jacomb <tim.jacomb@hmcts.net>
2021-01-22 08:22:18 +0000

    Slight cleanup

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {helpRequestRaised} = require("./src/messages");
+const {helpRequestRaised, helloToBotHandler, openHelpRequestBlocks} = require("./src/messages");
 const {App, LogLevel, SocketModeReceiver} = require('@slack/bolt');
 const crypto = require('crypto')
 
@@ -28,24 +28,13 @@ app.event('app_home_opened', async ({event, client}) => {
                     "block_id": "section678",
                     "text": {
                         "type": "mrkdwn",
-                        "text": "App Home Published via web sockets in glitch"
+                        "text": "App Home Published via web sockets"
                     },
                 }
             ]
         },
     });
 });
-
-function option(name, option) {
-    return {
-        text: {
-            type: "plain_text",
-            text: name,
-            emoji: true
-        },
-        value: option ?? name.toLowerCase()
-    }
-}
 
 // Message Shortcut example
 app.shortcut('launch_msg_shortcut', async ({shortcut, body, ack, context, client}) => {
@@ -64,148 +53,7 @@ app.shortcut('launch_shortcut', async ({shortcut, body, ack, context, client}) =
         // Call the views.open method using one of the built-in WebClients
         const result = await client.views.open({
             trigger_id: shortcut.trigger_id,
-            view: {
-                "title": {
-                    "type": "plain_text",
-                    "text": "Platform help request"
-                },
-                "submit": {
-                    "type": "plain_text",
-                    "text": "Submit"
-                },
-                "blocks": [
-                    {
-                        "type": "input",
-                        "block_id": "summary",
-                        "element": {
-                            "type": "plain_text_input",
-                            "action_id": "title",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Short description of the issue"
-                            }
-                        },
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Issue summary"
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "block_id": "urls",
-                        "optional": true,
-                        "element": {
-                            "type": "plain_text_input",
-                            "action_id": "title",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Link to any build or pull request"
-                            }
-                        },
-                        "label": {
-                            "type": "plain_text",
-                            "text": "PR / build URLs"
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "block_id": "environment",
-                        "optional": true,
-                        "element": {
-                            "type": "static_select",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Choose an environment",
-                                "emoji": true
-                            },
-                            "options": [
-                                option('AAT / Staging', 'staging'),
-                                option('Preview / Dev', 'dev'),
-                                option('Production'),
-                                option('Perftest / Test', 'test'),
-                                option('ITHC'),
-                                option('N/A', 'none')
-                            ],
-                            "action_id": "environment"
-                        },
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Environment",
-                            "emoji": true
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "block_id": "description",
-                        "element": {
-                            "type": "plain_text_input",
-                            "multiline": true,
-                            "action_id": "description"
-                        },
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Issue description",
-                            "emoji": true
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "block_id": "analysis",
-                        "element": {
-                            "type": "plain_text_input",
-                            "multiline": true,
-                            "action_id": "analysis"
-                        },
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Analysis done so far",
-                            "emoji": true
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "block_id": "checked_with_team",
-                        "element": {
-                            "type": "static_select",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Select an item",
-                                "emoji": true
-                            },
-                            "options": [
-                                option('No'),
-                                option('Yes')
-                            ],
-                            "action_id": "checked_with_team"
-                        },
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Have you checked with your team?",
-                            "emoji": true
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "block_id": "action_required",
-                        "optional": true,
-                        "element": {
-                            "type": "plain_text_input",
-                            "action_id": "plain_text_input-action",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "If you know what needs doing let us know"
-                            }
-                        },
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Action required",
-                            "emoji": true
-                        }
-                    }
-                ],
-                "type": "modal",
-                callback_id: 'create_help_request'
-            }
+            view: openHelpRequestBlocks()
         });
     } catch (error) {
         console.error(JSON.stringify(error));
@@ -272,27 +120,7 @@ app.event('app_mention', async ({event, context, client, say}) => {
 app.message('hello', async ({message, say}) => {
     // say() sends a message to the channel where the event was triggered
     // no need to directly use 'chat.postMessage', no need to include token
-    await say({
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `Thanks for the mention <@${message.user}>! Click my fancy button`
-                },
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Button",
-                        "emoji": true
-                    },
-                    "value": "click_me_123",
-                    "action_id": "first_button"
-                }
-            }
-        ]
-    });
+    await helloToBotHandler({message, say});
 });
 
 // Listen and respond to button click
@@ -329,15 +157,15 @@ app.action('assign_help_request_to_me', async ({
 })
 
 app.action('resolve_help_request', async ({
-                                                   body, action, ack, client, context
-                                               }) => {
+                                              body, action, ack, client, context
+                                          }) => {
     await ack();
     console.log('TODO implement resolve_help_request')
 });
 
 app.action('assign_help_request_to_user', async ({
-                                              body, action, ack, client, context
-                                          }) => {
+                                                     body, action, ack, client, context
+                                                 }) => {
     await ack();
     console.log('TODO implement assign_help_request_to_user')
 });
--- src/messages.js
+++ src/messages.js
@@ -85,4 +85,187 @@ function helpRequestRaised(user, summary, environment, assignedTo, jiraId) {
     ]
 }
 
-module.exports.helpRequestRaised = helpRequestRaised
+function option(name, option) {
+    return {
+        text: {
+            type: "plain_text",
+            text: name,
+            emoji: true
+        },
+        value: option ?? name.toLowerCase()
+    }
+}
+
+function openHelpRequestBlocks() {
+    return {
+        "title": {
+            "type": "plain_text",
+            "text": "Platform help request"
+        },
+        "submit": {
+            "type": "plain_text",
+            "text": "Submit"
+        },
+        "blocks": [
+            {
+                "type": "input",
+                "block_id": "summary",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "title",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Short description of the issue"
+                    }
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Issue summary"
+                }
+            },
+            {
+                "type": "input",
+                "block_id": "urls",
+                "optional": true,
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "title",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Link to any build or pull request"
+                    }
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "PR / build URLs"
+                }
+            },
+            {
+                "type": "input",
+                "block_id": "environment",
+                "optional": true,
+                "element": {
+                    "type": "static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Choose an environment",
+                        "emoji": true
+                    },
+                    "options": [
+                        option('AAT / Staging', 'staging'),
+                        option('Preview / Dev', 'dev'),
+                        option('Production'),
+                        option('Perftest / Test', 'test'),
+                        option('ITHC'),
+                        option('N/A', 'none')
+                    ],
+                    "action_id": "environment"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Environment",
+                    "emoji": true
+                }
+            },
+            {
+                "type": "input",
+                "block_id": "description",
+                "element": {
+                    "type": "plain_text_input",
+                    "multiline": true,
+                    "action_id": "description"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Issue description",
+                    "emoji": true
+                }
+            },
+            {
+                "type": "input",
+                "block_id": "analysis",
+                "element": {
+                    "type": "plain_text_input",
+                    "multiline": true,
+                    "action_id": "analysis"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Analysis done so far",
+                    "emoji": true
+                }
+            },
+            {
+                "type": "input",
+                "block_id": "checked_with_team",
+                "element": {
+                    "type": "static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select an item",
+                        "emoji": true
+                    },
+                    "options": [
+                        option('No'),
+                        option('Yes')
+                    ],
+                    "action_id": "checked_with_team"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Have you checked with your team?",
+                    "emoji": true
+                }
+            },
+            {
+                "type": "input",
+                "block_id": "action_required",
+                "optional": true,
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "plain_text_input-action",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "If you know what needs doing let us know"
+                    }
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Action required",
+                    "emoji": true
+                }
+            }
+        ],
+        "type": "modal",
+        callback_id: 'create_help_request'
+    }
+
+}
+
+async function helloToBotHandler({message, say}) {
+    await say({
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `Thanks for the mention <@${message.user}>! Click my fancy button`
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Button",
+                        "emoji": true
+                    },
+                    "value": "click_me_123",
+                    "action_id": "first_button"
+                }
+            }
+        ]
+    })
+}
+
+module.exports.helpRequestRaised = helpRequestRaised;
+module.exports.helloToBotHandler = helloToBotHandler;
+module.exports.openHelpRequestBlocks = openHelpRequestBlocks;
