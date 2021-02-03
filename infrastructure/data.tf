b3a1751ba72c68a84b69ed1a3dde95052fc9d127
Hannah Smith <13995068+hannah38@users.noreply.github.com>
2021-02-03 17:34:14 +0000

    Tf name fixes (#4)

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,8 +1,8 @@
 data "azurerm_client_config" "current" {
 }
 
-data "azurerm_key_vault" "mgmt-kv" {
-  name                = "cftptl-intsvc"
+data "azurerm_key_vault" "mgmt_kv" {
+  name                = var.environment
   resource_group_name = "core-infra-intsvc-rg"
 }
 
--- infrastructure/identity.tf
+++ infrastructure/identity.tf
@@ -8,7 +8,7 @@ resource "azurerm_user_assigned_identity" "managed_identity" {
 }
 
 resource "azurerm_key_vault_access_policy" "implicit_managed_identity_access_policy" {
-  key_vault_id = data.azurerm_key_vault.mgmt-kv.id
+  key_vault_id = data.azurerm_key_vault.mgmt_kv.id
 
   object_id = azurerm_user_assigned_identity.managed_identity.principal_id
   tenant_id = data.azurerm_client_config.current.tenant_id
