3f61fc0edbcd5c70e82ca4302aa834bff1d1e3a6
Jakub Kozłowski <jakub.kozlowski@gmail.com>
2021-06-17 13:07:09 +0100

    Add display name to FPRL
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- src/messages.js
+++ src/messages.js
@@ -427,7 +427,7 @@ function openHelpRequestBlocks() {
                         option('Expert UI', 'xui'),
                         option('Financial Remedy', 'finrem'),
                         option('FPLA'),
-                        option('FPRL'),
+                        option('Family Private Law', 'FPRL'),
                         option('Heritage'),
                         option('HMI'),
                         option('Management Information', 'mi'),
