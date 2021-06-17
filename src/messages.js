5e6158579be24dc52c2e0a121088520913a978d9
Patryk Jar <jar.patryk@gmail.com>
2021-06-17 13:22:45 +0200

    Add Family Private Law (FPRL) team

--- src/messages.js
+++ src/messages.js
@@ -427,6 +427,7 @@ function openHelpRequestBlocks() {
                         option('Expert UI', 'xui'),
                         option('Financial Remedy', 'finrem'),
                         option('FPLA'),
+                        option('FPRL'),
                         option('Heritage'),
                         option('HMI'),
                         option('Management Information', 'mi'),
