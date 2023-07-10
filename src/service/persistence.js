c5fe823bc83091a831f3284d3b64bb3cf6fb56f5
Dan Lysiak <danlysiak@hotmail.com>
2023-07-10 16:31:57 +0100

    Add default epic

--- src/service/persistence.js
+++ src/service/persistence.js
@@ -169,7 +169,8 @@ async function createHelpRequestInJira(summary, project, user, labels) {
             description: undefined,
             reporter: {
                 name: user // API docs say ID, but our jira version doesn't have that field yet, may need to change in future
-            }
+            },
+            customfield_10008: 'PAY-6381' // TODO: Probably make configurable
         }
     });
 }
