785e1c0b3d4a46bf8498ccc38c5597f581fae05b
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-08-08 14:19:37 +0100

    Update terraform-lint.tests.ps1

--- ci/terraform-lint.tests.ps1
+++ ci/terraform-lint.tests.ps1
@@ -22,7 +22,7 @@ else
     Context "Do not contain the 'Owner' role" {
       It "<Instance> does not have owner role" -TestCases $TfTestCases {
           Param($Instance)
-           ((Get-Content -raw $Instance) | Select-String -Pattern "role_definition_name.*=.*Owner" ) | should BeNullOrEmpty            
+           ((Get-Content -raw $Instance) | Select-String -Pattern "role_definition_name.*=.*Owner" ) | should -BeNullOrEmpty            
       }
     }
 
@@ -31,7 +31,7 @@ else
       Context "Are correctly formatted" {
         It "All files in <Instance> are correctly formatted" -TestCases $TfFolderTestCases {
           Param($Instance)
-          Invoke-Expression "terraform fmt -check=true $Instance"  | should BeNullOrEmpty            
+          Invoke-Expression "terraform fmt -check=true $Instance"  | should -BeNullOrEmpty            
       }
     }
   }
