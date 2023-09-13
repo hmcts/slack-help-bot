bb47e384613e669647d57b4583f18c894c17e52a
Dan Lysiak <danlysiak@hotmail.com>
2023-09-13 20:15:40 +0100

    Add remaining CCD services for service status checks

--- src/service/serviceStatus.js
+++ src/service/serviceStatus.js
@@ -11,8 +11,16 @@ const ITHC = 'ithc';
 const refreshDelay = 15;
 const serviceTemplates = {
     "ccd": [
+        new ServiceTemplate("ccd-admin-web", env => prodOverride(env, `https://ccd-admin-web.platform.hmcts.net`, `https://ccd-admin-web.${env}.platform.hmcts.net`)),
+        new ServiceTemplate("ccd-api-gateway-web", env => prodOverride(env, `https://gateway.ccd.platform.hmcts.net`, `https://gateway-ccd.${env}.platform.hmcts.net`)),
+        new ServiceTemplate("ccd-case-activity-api", env => `http://ccd-case-activity-api-${env}.service.core-compute-${env}.internal`),
+        new ServiceTemplate("ccd-case-document-am-api", env => `http://ccd-case-document-am-api-${env}.service.core-compute-${env}.internal`),
+        new ServiceTemplate("ccd-case-print-service", env => prodOverride(env, `https://return-case-doc.ccd.platform.hmcts.net`, `https://return-case-doc-ccd.${env}.platform.hmcts.net`)),
         new ServiceTemplate("ccd-data-store-api", env => `http://ccd-data-store-api-${env}.service.core-compute-${env}.internal`),
-        new ServiceTemplate("ccd-definition-store-api", env => `http://ccd-definition-store-api-${env}.service.core-compute-${env}.internal`)
+        new ServiceTemplate("ccd-definition-store-api", env => `http://ccd-definition-store-api-${env}.service.core-compute-${env}.internal`),
+        new ServiceTemplate("ccd-message-publisher", env => `http://ccd-message-publisher-${env}.service.core-compute-${env}.internal`),
+        new ServiceTemplate("ccd-test-stubs-service", env => `http://ccd-test-stubs-service-${env}.service.core-compute-${env}.internal`, [ DEMO, AAT, ITHC, PERFTEST ]),
+        new ServiceTemplate("ccd-user-profile-api", env => `http://ccd-user-profile-api-${env}.service.core-compute-${env}.internal`)
     ],
     "xui": [
         new ServiceTemplate("xui-webapp", env => prodOverride(env, `https://manage-case.platform.hmcts.net`, `https://manage-case.${env}.platform.hmcts.net`)),
@@ -26,7 +34,6 @@ const services = {
     'DEMO': getServices(DEMO),
     'PROD': getServices(PROD)
 }
-console.log(services)
 
 function prodOverride(env, prodUrl, defaultTemplateUrl) {
     return env === PROD ? prodUrl : defaultTemplateUrl;
