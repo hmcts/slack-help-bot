525bc379ac34b36b1e8ed7f99022182c8b9a2044
Tim Jacomb <timjacomb1@gmail.com>
2024-07-18 14:40:59 +0100

    Make compatible with ADO

--- components/infrastructure/app-insights.tf
+++ components/infrastructure/app-insights.tf
@@ -1,5 +1,5 @@
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source = "git::https://github.com/hmcts/terraform-module-application-insights.git?ref=main"
 
   product = var.product
   env     = var.short_environment
