f175f8ead99845162700e34539521f5cb45a38ee
Tim Jacomb <21194782+timja@users.noreply.github.com>
2024-07-22 09:42:57 +0100

    Add indexing of cosmos (#361)

---
+++ components/infrastructure/ai-search-indexer.tf
@@ -0,0 +1,20 @@
+locals {
+  indexer_json = {
+    name : "help-requests-indexer",
+    dataSourceName : "help-requests",
+    targetIndexName : "help-requests",
+    schedule : {
+      interval : "PT5M",
+      startTime : "2024-07-19T15:32:09.422Z"
+    },
+  }
+}
+
+// https://learn.microsoft.com/en-us/rest/api/searchservice/preview-api/create-or-update-indexer
+resource "restapi_object" "create_indexer" {
+  path         = "/indexers"
+  query_string = "api-version=2023-10-01-Preview"
+  data         = jsonencode(local.indexer_json)
+  id_attribute = "name" # The ID field on the response
+  depends_on   = [azurerm_search_service.this, restapi_object.help_request_index, restapi_object.search_datasource]
+}
