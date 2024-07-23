2c211640e49241a25164e5192c4e7c2598e341f7
Tim Jacomb <timjacomb1@gmail.com>
2024-07-23 11:02:03 +0100

    Handle email not being found in Jira

--- src/service/persistence.js
+++ src/service/persistence.js
@@ -55,12 +55,16 @@ async function convertEmail(email) {
   }
 
   try {
-    res = await jira.searchUsers(
-      (options = {
+    const res = await jira.searchUsers({
         username: email,
         maxResults: 1,
-      }),
+      },
     );
+
+    if (!res || res.length === 0) {
+      console.log("Failed to find user in Jira with email: " + email);
+      return undefined
+    }
 
     return res[0].name;
   } catch (ex) {
