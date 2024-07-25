0b226320290ad7d654af45df41fec89f030437e5
Tim Jacomb <timjacomb1@gmail.com>
2024-07-25 10:11:52 +0100

    Add link to region availability

--- components/infrastructure/ai-model.tf
+++ components/infrastructure/ai-model.tf
@@ -1,6 +1,7 @@
 resource "azurerm_cognitive_deployment" "model" {
   name                 = "gpt-4"
   cognitive_account_id = azapi_resource.AIServices.id
+  // region availability https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#gpt-4-and-gpt-4-turbo-model-availability
   model {
     format  = "OpenAI"
     name    = "gpt-4"
