16b8e16f5a26f6bc8a0107cfbf33ccb26bf3a151
Tim Jacomb <timjacomb1@gmail.com>
2024-08-02 16:53:01 +0100

    Add permission allowing hmcts way to trigger index

--- components/infrastructure/ai-search-hmcts-way-indexer.tf
+++ components/infrastructure/ai-search-hmcts-way-indexer.tf
@@ -14,3 +14,10 @@ resource "restapi_object" "hmcts_way_indexer" {
   id_attribute = "name" # The ID field on the response
   depends_on   = [azurerm_search_service.this, restapi_object.hmcts_way_index, restapi_object.storage_account_datasource]
 }
+
+resource "azurerm_role_assignment" "search_contributor_hmcts_way" {
+  principal_id = data.azuread_service_principal.hmcts_way_repo.id
+  scope        = azurerm_search_service.this.id
+
+  role_definition_name = "Search Service Contributor"
+}
