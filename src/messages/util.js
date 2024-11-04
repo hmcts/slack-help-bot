1f85c5c91e91b1b8ed90ba9cff571887d6ae8585
Hannah Smith <hannah.smith16@justice.gov.uk>
2024-11-04 17:36:49 +0000

    prettier

--- src/messages/util.js
+++ src/messages/util.js
@@ -34,7 +34,10 @@ function convertHighlightToSlackMarkup(highlight) {
 
 function extractKnowledgeStoreHighlight(item) {
   if (item.captions && item.captions.length > 0) {
-    return convertHighlightToSlackMarkup(item.captions[0].highlights) || "Generated highlight not available at this time";
+    return (
+      convertHighlightToSlackMarkup(item.captions[0].highlights) ||
+      "Generated highlight not available at this time"
+    );
   }
   return item.document.content.slice(0, 100);
 }
