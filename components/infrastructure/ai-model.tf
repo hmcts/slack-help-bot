2230baa79ea1e256de4fb8c7bbb6b462feb8e198
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2025-07-23 10:33:02 +0100

    Auto: Update AI model to gpt-4.1 2025-04-14 to avoid retirement

--- components/infrastructure/ai-model.tf
+++ components/infrastructure/ai-model.tf
@@ -1,17 +1,17 @@
 resource "azurerm_cognitive_deployment" "model" {
-  name                 = "gpt-4"
+  name                 = "gpt-4.1"
   cognitive_account_id = azurerm_ai_services.AIServices.id
   // region availability https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#gpt-4-and-gpt-4-turbo-model-availability
   model {
     format  = "OpenAI"
-    name    = "gpt-4o"
-    version = "2024-11-20"
+    name    = "gpt-4.1"
+    version = "2025-04-14"
   }
 
   sku {
-    name     = "Standard"
+    name     = "gpt-4.1"
     capacity = 36
   }
 
-  rai_policy_name = "Microsoft.DefaultV2"
+  rai_policy_name = "gpt-4.1"
 }
