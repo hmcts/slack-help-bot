40710c322938b3cfbf903a135266ea2a2e35f042
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2026-07-04 10:10:28 +0100

    Downgrade to latest available model on standard deployment (#759)

--- components/infrastructure/ai-model.tf
+++ components/infrastructure/ai-model.tf
@@ -1,11 +1,11 @@
 resource "azurerm_cognitive_deployment" "model" {
-  name                 = "gpt-4"
+  name                 = "gpt-5"
   cognitive_account_id = azurerm_ai_services.AIServices.id
   // region availability https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#gpt-4-and-gpt-4-turbo-model-availability
   model {
     format  = "OpenAI"
-    name    = "gpt-5.5"
-    version = "2026-04-24"
+    name    = "gpt-5.1"
+    version = "2025-11-13"
   }
 
   sku {
