46a609a52c53f2fa9794b810a00eb3ad207bf7b5
Niall King <niall.king@justice.gov.uk>
2026-08-20 11:51:30 +0100

    adding logs back

--- src/slackHandlers/appMention.js
+++ src/slackHandlers/appMention.js
@@ -205,6 +205,14 @@ async function handleRunbook({ event, client, helpRequestMessages, say }) {
   const searchQuery = `${summary}\n${description}`;
 
   const knowledgeStoreResults = await searchOpsRunbook(searchQuery);
+  console.log(
+    "ops-runbook search results:",
+    searchQuery,
+    knowledgeStoreResults.map((result) => ({
+      title: result.document?.title,
+      rerankerScore: result.rerankerScore,
+    })),
+  );
   const runbookAnswer = await answerFromRunbookKnowledgeStore(
     runbookPrompt,
     knowledgeStoreResults,
